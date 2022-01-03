81e61df9b029690c315581ed3bece73fefef5802
YariMorcus <yarimorcus@zeelandnet.nl>
2022-01-03 16:14:06 +0100

    Add DocBlock comment for plugin recognition within WP

---
+++ oliemonster-portal.php
@@ -0,0 +1,20 @@
+<?php 
+/**
+ * Plugin Name: Oliemonster portal
+ * Description: This plugin adds the oliemonster portal as a functionality to WordPress.
+ * Version: 0.1
+ * Requires at least: 5.8.2
+ * Requires PHP: 7.0.33
+ * Author: Yari Morcus
+ * Text Domain: oliemonster-portal
+ * Domain Path: languages
+ *  
+ * This is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details
+ * You should have received a copy of the GNU General Public License
+ * along with your plugin. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+?>

15e300be3bf593db945f9a80a0a6b54f7d7709e7
YariMorcus <yarimorcus@zeelandnet.nl>
2022-01-06 14:54:35 +0100

    Add required attr. to all input and select fields

--- includes/views/aanvragen_controle.php
+++ includes/views/aanvragen_controle.php
@@ -54,80 +54,80 @@ if ( !empty( $post_array ) ) {
                     <div class="form-group row mb-3">
                         <label for="monsternummer" class="col-sm-3 col-form-label">Monsternummer</label>
                         <div class="col-sm-9 col-lg-9 col-xl-6">
-                            <input name="monsternummer" type="number" readonly class="form-control-plain-text w-100" id="monsternummer" aria-describedby="monsternummer-help" value="1203232">
+                            <input name="monsternummer" type="number" required readonly class="form-control-plain-text w-100" id="monsternummer" aria-describedby="monsternummer-help" value="1203232">
                             <small id="monsternummer-help" class="form-text text-muted d-block">U kunt dit nummer niet wijzigen. Dit wordt automatisch toegekend.</small>
                         </div>
                     </div> <!-- .form-group -->
                     <div class="form-group row mb-3">
                         <label for="klantnaam" class="col-sm-3 col-form-label">Klant</label>
                         <div class="col-sm-9 col-lg-9 col-xl-6">
-                            <input name="klantnaam" type="text" class="form-control-plain-text w-100" id="klantnaam" placeholder="Vul hier uw naam in">
+                            <input name="klantnaam" type="text" required class="form-control-plain-text w-100" id="klantnaam" placeholder="Vul hier uw naam in">
                         </div>
                     </div> <!-- .form-group -->
                     <div class="form-group row mb-3">
                         <label for="Schipnaam" class="col-sm-3 col-form-label">Schip</label>
                         <div class="col-sm-9 col-lg-9 col-xl-6">
-                            <input name="Schipnaam" type="text" class="form-control-plain-text w-100" id="Schipnaam" placeholder="Vul hier de naam van uw schip in">
+                            <input name="Schipnaam" type="text" required class="form-control-plain-text w-100" id="Schipnaam" placeholder="Vul hier de naam van uw schip in">
                         </div>
                     </div> <!-- .form-group -->
                     <div class="form-group row mb-3">
                         <label for="motor" class="col-sm-3 col-form-label">Motor</label>
                         <div class="col-sm-9 col-lg-9 col-xl-6">
-                            <input name="motor" type="text" class="form-control-plain-text w-100" id="motor" placeholder="Vul hier uw motor in">
+                            <input name="motor" type="text" required class="form-control-plain-text w-100" id="motor" placeholder="Vul hier uw motor in">
                         </div>
                     </div> <!-- .form-group -->
                     <div class="form-group row mb-3">
                         <label for="type" class="col-sm-3 col-form-label">Type</label>
                         <div class="col-sm-9 col-lg-9 col-xl-6">
-                            <input name="type-motor" type="text" class="form-control-plain-text w-100" id="type" placeholder="Vul hier het type in">
+                            <input name="type-motor" type="text" required class="form-control-plain-text w-100" id="type" placeholder="Vul hier het type in">
                         </div>
                     </div> <!-- .form-group -->
                     <div class="form-group row mb-3">
                         <label for="serienummer" class="col-sm-3 col-form-label">Serienummer</label>
                         <div class="col-sm-9 col-lg-9 col-xl-6">
-                            <input name="serienummer" type="text" class="form-control-plain-text w-100" id="serienummer" placeholder="Vul hier het serienummer in">
+                            <input name="serienummer" type="text" required class="form-control-plain-text w-100" id="serienummer" placeholder="Vul hier het serienummer in">
                         </div>
                     </div> <!-- .form-group -->
                     <div class="form-group row mb-3">
                         <label for="soort-onderzoek" class="col-sm-3 col-form-label">Soort onderzoek</label>
                         <div class="col-sm-9 col-lg-9 col-xl-6">
-                            <input name="soort-onderzoek" type="text" class="form-control-plain-text w-100" id="soort-onderzoek" placeholder="Vul hier het soort onderzoek in">
+                            <input name="soort-onderzoek" type="text" required class="form-control-plain-text w-100" id="soort-onderzoek" placeholder="Vul hier het soort onderzoek in">
                         </div>
                     </div> <!-- .form-group -->
                     <div class="form-group row mb-3">
                         <label for="monster-datum" class="col-sm-3 col-form-label">Monster datum</label>
                         <div class="col-sm-9 col-lg-9 col-xl-6">
-                            <input name="monster-datum" type="date" class="form-control-plain-text w-100" id="monster-datum" placeholder="Vul hier de monster datum in">
+                            <input name="monster-datum" type="date" required class="form-control-plain-text w-100" id="monster-datum" placeholder="Vul hier de monster datum in">
                         </div>
                     </div> <!-- .form-group -->
                     <div class="form-group row mb-3">
                         <label for="urenstand-motor" class="col-sm-3 col-form-label">Urenstand motor</label>
                         <div class="col-sm-9 col-lg-9 col-xl-6">
-                            <input name="urenstand-motor" type="number" class="form-control-plain-text w-100" id="urenstand-motor" placeholder="Vul hier de urenstand in van de motor">
+                            <input name="urenstand-motor" type="number" required class="form-control-plain-text w-100" id="urenstand-motor" placeholder="Vul hier de urenstand in van de motor">
                         </div>
                     </div> <!-- .form-group -->
                     <div class="form-group row mb-3">
                         <label for="merk-olie" class="col-sm-3 col-form-label">Merk olie</label>
                         <div class="col-sm-9 col-lg-9 col-xl-6">
-                            <input name="merk-olie" type="text" class="form-control-plain-text w-100" id="merk-olie" placeholder="Vul hier het merk in van de olie">
+                            <input name="merk-olie" type="text" required class="form-control-plain-text w-100" id="merk-olie" placeholder="Vul hier het merk in van de olie">
                         </div>
                     </div> <!-- .form-group -->
                     <div class="form-group row mb-3">
                         <label for="type-olie" class="col-sm-3 col-form-label">Type olie</label>
                         <div class="col-sm-9 col-lg-9 col-xl-6">
-                            <input name="type-olie" type="text" class="form-control-plain-text w-100" id="type-olie" placeholder="Vul hier het type in van de olie">
+                            <input name="type-olie" type="text" required class="form-control-plain-text w-100" id="type-olie" placeholder="Vul hier het type in van de olie">
                         </div>
                     </div> <!-- .form-group -->
                     <div class="form-group row mb-3">
                         <label for="urengebruik-olie" class="col-sm-3 col-form-label">Urengebruik olie</label>
                         <div class="col-sm-9 col-lg-9 col-xl-6">
-                            <input name="urengebruik-olie" type="number" class="form-control-plain-text w-100" id="urengebruik-olie" placeholder="Vul hier het urengebruik in van de olie">
+                            <input name="urengebruik-olie" type="number" required class="form-control-plain-text w-100" id="urengebruik-olie" placeholder="Vul hier het urengebruik in van de olie">
                         </div>
                     </div> <!-- .form-group -->
                     <div class="form-group row mb-3">
                         <label for="olie-ververst" class="col-sm-3 col-form-label">Olie ververst</label>
                         <div class="col-sm-9 col-lg-9 col-xl-6">
-                            <select name="olie-ververst" id="olie-ververst" class="custom-select w-100">
+                            <select name="olie-ververst" id="olie-ververst" required class="custom-select w-100">
                                 <option value="">Selecteer ja of nee</option>
                                 <option value="1">Ja</option>
                                 <option value="2">Nee</option>
@@ -137,7 +137,7 @@ if ( !empty( $post_array ) ) {
                     <div class="form-group row mb-3">
                         <label for="filters-ververst" class="col-sm-3 col-form-label">Filters ververst</label>
                         <div class="col-sm-9 col-lg-9 col-xl-6">
-                            <select name="filters-ververst" id="filters-ververst" class="custom-select w-100">
+                            <select name="filters-ververst" id="filters-ververst" required class="custom-select w-100">
                                 <option value="">Selecteer ja of nee</option>
                                 <option value="1">Ja</option>
                                 <option value="2">Nee</option>
@@ -147,7 +147,7 @@ if ( !empty( $post_array ) ) {
                     <div class="form-group row mb-3">
                         <label for="koelmiddel-gebruikt" class="col-sm-3 col-form-label">Koelmiddel gebruikt</label>
                         <div class="col-sm-9 col-lg-9 col-xl-6">
-                            <select name="koelmiddel-gebruikt" id="koelmiddel-gebruikt" class="custom-select w-100">
+                            <select name="koelmiddel-gebruikt" id="koelmiddel-gebruikt" required class="custom-select w-100">
                                 <option value="">Selecteer ja of nee</option>
                                 <option value="1">Ja</option>
                                 <option value="2">Nee</option>
@@ -157,7 +157,7 @@ if ( !empty( $post_array ) ) {
                     <div class="form-group row mb-3">
                         <label for="merk-koelmiddel" class="col-sm-3 col-form-label">Merk koelmiddel</label>
                         <div class="col-sm-9 col-lg-9 col-xl-6">
-                            <input name="merk-koelmiddel" type="text" class="form-control-plain-text w-100" id="merk-koelmiddel" placeholder="Vul hier het merk van uw koelmiddel in">
+                            <input name="merk-koelmiddel" type="text" required class="form-control-plain-text w-100" id="merk-koelmiddel" placeholder="Vul hier het merk van uw koelmiddel in">
                         </div>
                     </div> <!-- .form-group -->
                     <div class="form-group row mb-3">

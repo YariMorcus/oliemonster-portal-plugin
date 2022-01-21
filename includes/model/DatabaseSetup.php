f8cb895348d6554234f2ee8c754d88bcf9a52227
YariMorcus <yarimorcus@zeelandnet.nl>
2022-01-21 09:25:59 +0100

    Remove comment (code review)

--- includes/model/DatabaseSetup.php
+++ includes/model/DatabaseSetup.php
@@ -180,45 +180,6 @@ class DatabaseSetup {
 
     }
 
-    /**
-     * removeDBTables
-     * 
-     * Remove the automatically created tables when plugin is deactivated
-    */
-    // public static function removeDBTables() {
-
-    //     global $wpdb;
-
-    //     // Include the IVS_Databases class
-    //     require_once IVS_MEELOOP_PORTAAL_PLUGIN_INCLUDES_MODEL_DIR . '/IVS_Databases.php';
-
-    //     // Retrieve the table names
-    //     $table_names = IVS_Databases::retrieveTables();
-
-    //     // Loop over each $table
-    //     foreach( $table_names as $table ) {
-
-    //         // Delete query
-    //         $query = "DROP TABLE IF EXISTS $table";
-
-    //         // Bypass queries in order for all tables to be deleted
-    //         // If not supplied, table 'ivs_mp_email_status' won't be deleted because of foreign key referencing this table
-    //         $disable_foreign_key_check = "SET foreign_key_checks = 0;";
-    //         $enable_foreign_key_check = "SET foreign_key_checks = 1;";
-
-    //         // Disable the foreign key check
-    //         $wpdb->query ( $disable_foreign_key_check );
-
-    //         // Delete tables
-    //         $wpdb->query( $query );
-
-    //         // Enable foreign key check to maintain integrity of database
-    //         $wpdb->query( $enable_foreign_key_check );
-
-    //     }
-        
-    // }
-
 }
 
 ?>

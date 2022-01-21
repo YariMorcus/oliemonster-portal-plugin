b1007c76134093a19785a6e26baa0914d8219504
YariMorcus <yarimorcus@zeelandnet.nl>
2022-01-21 09:27:09 +0100

    Remove comments (code review)

--- admin/OliemonsterPortal_AdminController.php
+++ admin/OliemonsterPortal_AdminController.php
@@ -101,26 +101,6 @@ class OliemonsterPortal_AdminController {
             array( 'OliemonsterPortal_AdminController', 'viewDataRequestCheckPage' )
 
         );
-
-        // Add menu page for subscribers only
-        // add_menu_page(
-        //     // $page_title, The text to be displayed in the title tags of the page when the menu is selected
-        //     __( 'Oliemonster portal dashboard', 'oliemonster-portal' ),
-        //     // $menu_title, The text to be used for the menu.
-        //     __( 'Portal dashboard', 'oliemonster-portal' ),
-        //     // $capability, The capability required for this menu to be displayed to the user
-        //     'read',
-        //     // $menu_slug, The slug name to refer to this menu by. Should be unique for this menu page
-        //     'oliemonster-portal-dashboard',
-        //     // $function, The function to be called to output the content for this page
-        //     array( 'OliemonsterPortal_AdminController', 'subscriberDashboardPage' ),
-        //     // $icon_url, The URL to the icon to be used for this menu.
-        //     // Pass a base64-encoded SVG using a data URI, which will be colored to match the color scheme. This should begin with 'data:image/svg+xml;base64,'.
-        //     // Pass the name of a Dashicons helper class to use a font icon, e.g. 'dashicons-chart-pie'.
-        //     // Pass 'none' to leave div.wp-menu-image empty so an icon can be added via CSS.
-        //     'dashicons-dashboard'
-        // );
-
         
     }
 
@@ -163,15 +143,6 @@ class OliemonsterPortal_AdminController {
         include OLIEMONSTER_PORTAL_PLUGIN_ADMIN_VIEWS_DIR . '/admin_inzien_gegevens.php';
 
     }
-
-    /**
-     * subscriberDashboardPage
-    */
-    // static function subscriberDashboardPage() {
-
-    //     // Include the view for this menu page
-    //     include OLIEMONSTER_PORTAL_PLUGIN_ADMIN_VIEWS_DIR . '/subscriber_dashboard.php';
-    // }
 
 }
 
